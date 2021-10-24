f1755d4df249a1af60644fcc57154459cdfdb268
Kevin Grace <kgrace90@yahoo.com>
2021-10-24 16:56:21 -0400

    additional controller

--- models/User.js
+++ models/User.js
@@ -1,6 +1,40 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema ()
+const UserSchema = new Schema (
+    // Schema data for user
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        thoughts: {
+            type: Schema.Types.ObjectOd,
+            ref: 'Thought'
+        },
+        friends: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 
 const User - model ('User', UserSchema);
 
